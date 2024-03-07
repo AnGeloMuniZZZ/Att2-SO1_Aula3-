041e09582e5194864d8633d0c5c0db21aa8b36ce
angel <angel@DESKTOP-S83E2U3>
2024-03-07 00:40:19 -0300

    Atualizacao

--- src/controller/KillController.java
+++ src/controller/KillController.java
@@ -1,5 +1,70 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class KillController {
 
+	public KillController() {
+		super();
+	}
+
+	private String OS() {
+		return System.getProperty("os.name");
+	}
+
+	public void ListaProcessos() {
+		Process p;
+		try {
+			if (this.OS().contains("Windows")) {
+				p = Runtime.getRuntime().exec("TASKLIST /FO TABLE");
+				InputStream dados = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(dados);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					System.out.println(linha);
+					linha = buffer.readLine();
+				}
+			} else {
+				p = Runtime.getRuntime().exec("ps -ef");
+				InputStream dados = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(dados);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					System.out.println(linha);
+					linha = buffer.readLine();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void mataPid(int PID) {
+		try {
+			if (this.OS().contains("Windows")) {
+				Runtime.getRuntime().exec("TASKKILL /PID" + " " + PID);
+			} else {
+				Runtime.getRuntime().exec("kill -9" + " " + PID);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	public void mataNome(String nome) {
+		try {
+			if (this.OS().contains("Windows")) {
+				Runtime.getRuntime().exec("TASKKILL /IM" + " " + nome);
+			} else {
+				Runtime.getRuntime().exec("pkill -f" + " " + nome);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,29 @@
 package view;
 
+import javax.swing.JOptionPane;
+
+import controller.KillController;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		KillController kill = new KillController();
+		kill.ListaProcessos();
+		String i = JOptionPane.showInputDialog(null, "1- Mata com PID\n 2- Mata com Nome", JOptionPane.PLAIN_MESSAGE);
+		switch (Integer.parseInt(i)) {
+		case 1: {
+			kill.mataPid(Integer.parseInt(JOptionPane.showInputDialog("Coloque o número do PID")));
+			break;
+		}
+		case 2:{
+			kill.mataNome(JOptionPane.showInputDialog("Coloque o nome do processo"));
+			break;
+		}
+		default:
+			break;
+		}
+		
 	}
 
 }
